99d994300ce4d53c1a95b07c8991fa21fd5eeb28
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2022-10-15 10:40:28 +0100

    Edit Api

--- src/Api.js
+++ src/Api.js
@@ -1,2 +1,2 @@
 export const JOBS_API = 'https://job-board-mern-api.onrender.com/api/jobs';
-export const USERS_API = 'http://localhost:4000/api/user';
+export const USERS_API = 'http://job-board-mern-api.onrender.com/api/user';
